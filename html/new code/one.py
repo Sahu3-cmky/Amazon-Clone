4f13fdf4db46c247f205caa125b15f0da97bc093
Aman-98-cyber <2k23.csaiml2313774@gmail.com>
2024-10-20 14:35:23 +0530

    update

--- html/new code/one.py
+++
@@ -1,7 +0,0 @@
-def take_n_natural_numbers(n):
-    natural_numbers = list(range(1, n + 1))
-    return natural_numbers
-n = int(input("Enter the number of natural numbers: "))
-numbers = take_n_natural_numbers(n)
-
-print("The first", n, "natural numbers are:", numbers)
